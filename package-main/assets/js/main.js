6a3d06252b649802114fd014766a967891bb7c77
quocvietht03 <quocvietht03@gmail.com>
2023-08-21 16:19:09 +0700

    update single deal with new requirement

--- package-main/assets/js/main.js
+++ package-main/assets/js/main.js
@@ -2,11 +2,20 @@
   'use strict';
 
   var swiper = new Swiper('.swiper', {
-    slidesPerView: 2,
-    spaceBetween: 20,
+    slidesPerView: 1,
+    spaceBetween: 0,
     loop: true,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      }
+    },
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
     },
   });
 
--- single-deal.php
+++ single-deal.php
@@ -54,53 +54,14 @@
 
 	<main class="deal-main">
 		<section class="deal-hero-ss">
-			<div class="container">
-				<div class="deal-hero-content">
-					<h1 class="deal-page-title">
-						<?php echo $page_title; ?>
-					</h1>
-				</div>
-			</div>
+			<?php
+				if ( has_post_thumbnail() ) :
+					the_post_thumbnail( 'full' );
+				endif;
+			?>
 		</section>
 
-		<section class="deal-slider-ss<?php if ( have_rows( 'image_slider' ) ) echo ' has-slider' ?>">
-			<div class="container">
-				<div class="deal-slider-content">
-					<?php if ( have_rows( 'image_slider' ) ) : ?>
-						<div class="swiper">
-						  <div class="swiper-wrapper">
-								<?php if ( has_post_thumbnail() ) : ?>
-									<div class="swiper-slide">
-										<div class="cover-image">
-											<?php the_post_thumbnail( 'deal' ); ?>
-										</div>
-									</div>
-								<?php endif; ?>
-
-								<?php while ( have_rows( 'image_slider' ) ) : the_row(); ?>
-									<div class="swiper-slide">
-										<div class="cover-image">
-											<?php echo wp_get_attachment_image( get_sub_field( 'image' ), 'deal' ); ?>
-										</div>
-									</div>
-								<?php endwhile; ?>
-						  </div>
-							<div class="swiper-pagination"></div>
-						</div>
-					<?php elseif ( has_post_thumbnail() ) : ?>
-						<div class="cover-image">
-							<?php the_post_thumbnail( 'deal' ); ?>
-						</div>
-					<?php else : ?>
-						<div class="cover-image">
-							<img src="<?php echo PJ_URI . 'assets/images/deal-placeholder.jpg'; ?>" alt="" />
-						</div>
-					<?php endif; ?>
-				</div>
-			</div>
-		</section>
-
-		<section class="deal-header-ss">
+		<!-- <section class="deal-header-ss">
 			<div class="container">
 				<div class="deal-header-content">
 					<div class="deal-col-left">
@@ -134,81 +95,167 @@
 						<div class="deal-feature deal-price">
 							From <span>$<?php echo $price; ?><?php if ( $price_tax ) : ?>inc taxes<?php endif; ?></span>
 						</div>
-						<div>
+						<div class="deal-enquiry">
 							<a class="btn-enquiry" onclick="rbt_setBookingInfo();" href="<?php echo site_url( '/airfare-deals/price-my-airfare/' ); ?>">Enquire Now</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section> -->
+
+		<section class="deal-main-ss">
+			<div class="container">
+				<div class="deal-main-wrap">
+					<div class="deal-main-content">
+						<h3 class="deal-title"><?php the_title(); ?></h3>
+						<?php if ( get_field( 'subtitle' ) ) : ?>
+							<div class="deal-subtitle"><?php the_field( 'subtitle' ); ?></div>
+						<?php endif; ?>
+						<div class="deal-summary">
+								<?php the_field( 'summary' ); ?>
+						</div>
+						<div class="deal-post-content post-content-format">
+							<?php the_content(); ?>
+						</div>
+						<div class="deal-note">
+							<h3 class="deal-note-title">Note</h3>
+							<div class="deal-note-list">
+								<?php if ( !empty( $seasons ) ) : ?>
+									<div class="deal-note-item">
+										<span class="label">Seasons:</span>
+										<span class="value"><?php echo $seasons; ?></span>
+									</div>
+								<?php endif; ?>
+								<?php if ( !empty( $departure ) ) : ?>
+									<div class="deal-note-item">
+										<span class="label">Departure ports:</span>
+										<span class="value"><?php echo $departure; ?></span>
+									</div>
+								<?php endif; ?>
+								<?php if ( !empty( $book_by ) ) : ?>
+									<div class="deal-note-item">
+										<span class="label">Book by:</span>
+										<span class="value"><?php echo $book_by; ?></span>
+									</div>
+								<?php endif; ?>
+								<?php if ( !empty( $deposit ) ) : ?>
+									<div class="deal-note-item">
+										<span class="label">Deposit to book:</span>
+										<span class="value"><?php echo $deposit; ?></span>
+									</div>
+								<?php endif; ?>
+								<?php if ( !empty( $pay_by ) ) : ?>
+									<div class="deal-note-item">
+										<span class="label">Full payment by:</span>
+										<span class="value"><?php echo $pay_by; ?></span>
+									</div>
+								<?php endif; ?>
+								<?php if ( !empty( $change_fee ) ) : ?>
+									<div class="deal-note-item">
+										<span class="label">Change fee:</span>
+										<span class="value"><?php echo $change_fee; ?></span>
+									</div>
+								<?php endif; ?>
+								<?php if ( !empty( $lead_in_price ) ) : ?>
+									<div class="deal-note-item">
+										<span class="label">Lead in price:</span>
+										<span class="value"><?php echo $lead_in_price; ?></span>
+									</div>
+								<?php endif; ?>
+								<?php if ( !empty( $baggage ) ) : ?>
+									<div class="deal-note-item">
+										<span class="label">Baggage:</span>
+										<span class="value"><?php echo $baggage; ?></span>
+									</div>
+								<?php endif; ?>
+							</div>
+						</div>
+
+						<div class="deal-share">
+							<?php echo do_shortcode( '[addtoany]' ); ?>
+						</div>
+					</div>
+					<div class="deal-main-sidebar">
+						<div class="deal-sticky-sidebar">
+							<div class="deal-enquiry-form">
+								<div class="deal-feature-list">
+									<?php if ( !empty( $airline_titles ) ) : ?>
+										<div class="deal-feature-item deal-airl">
+											<span class="label">Airline:</span>
+											<span class="value"><?php echo implode( ', ', $airline_titles ); ?></span>
+										</div>
+									<?php endif; ?>
+									<?php if ( !empty( $destination_titles ) ) : ?>
+										<div class="deal-feature-item  deal-dest">
+											<span class="label">Destination:</span>
+											<span class="value"><?php echo implode( ', ', $destination_titles ); ?></span>
+										</div>
+									<?php endif; ?>
+									<?php if ( !empty( $class_titles ) ) : ?>
+										<div class="deal-feature-item deal-class">
+											<span class="label">Class:</span>
+											<span class="value"><?php echo implode( ', ', $class_titles ); ?></span>
+										</div>
+									<?php endif; ?>
+									<?php if ( !empty($price) ) : ?>
+										<div class="deal-feature-item deal-price">
+											From <span>$<?php echo $price; ?><?php if ( $price_tax ) : ?>inc taxes<?php endif; ?></span>
+										</div>
+									<?php endif; ?>
+								</div>
+
+								<?php echo do_shortcode('[contact-form-7 id="9672" title="Enquiry Form"]'); ?>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</section>
 
-		<section class="deal-main-ss">
-			<div class="container">
-				<div class="deal-main-content">
-					<div class="deal-post-content post-content-format">
-						<?php the_content(); ?>
-					</div>
-					<div class="deal-note">
-						<h3 class="deal-note-title">Note</h3>
-						<div class="deal-note-list">
-							<?php if ( !empty( $seasons ) ) : ?>
-								<div class="deal-note-item">
-									<span>Seasons:</span>
-									<?php echo $seasons; ?>
-								</div>
-							<?php endif; ?>
-							<?php if ( !empty( $departure ) ) : ?>
-								<div class="deal-note-item">
-									<span>Departure ports:</span>
-									<?php echo $departure; ?>
-								</div>
-							<?php endif; ?>
-							<?php if ( !empty( $book_by ) ) : ?>
-								<div class="deal-note-item">
-									<span>Book by:</span>
-									<?php echo $book_by; ?>
-								</div>
-							<?php endif; ?>
-							<?php if ( !empty( $deposit ) ) : ?>
-								<div class="deal-note-item">
-									<span>Deposit to book:</span>
-									<?php echo $deposit; ?>
-								</div>
-							<?php endif; ?>
-							<?php if ( !empty( $pay_by ) ) : ?>
-								<div class="deal-note-item">
-									<span>Full payment by:</span>
-									<?php echo $pay_by; ?>
-								</div>
-							<?php endif; ?>
-							<?php if ( !empty( $change_fee ) ) : ?>
-								<div class="deal-note-item">
-									<span>Change fee:</span>
-									<?php echo $change_fee; ?>
-								</div>
-							<?php endif; ?>
-							<?php if ( !empty( $lead_in_price ) ) : ?>
-								<div class="deal-note-item">
-									<span>Lead in price:</span>
-									<?php echo $lead_in_price; ?>
-								</div>
-							<?php endif; ?>
-							<?php if ( !empty( $baggage ) ) : ?>
-								<div class="deal-note-item">
-									<span>Baggage:</span>
-									<?php echo $baggage; ?>
-								</div>
-							<?php endif; ?>
-						</div>
-					</div>
+		<?php if ( have_rows( 'image_slider' ) ) : ?>
+			<section class="deal-slider-ss">
+				<div class="fullwidth">
+					<div class="deal-slider-content">
+						<?php if ( have_rows( 'image_slider' ) ) : ?>
+							<div class="swiper">
+							  <div class="swiper-wrapper">
+									<?php if ( has_post_thumbnail() ) : ?>
+										<div class="swiper-slide">
+											<div class="cover-image">
+												<?php the_post_thumbnail( 'deal' ); ?>
+											</div>
+										</div>
+									<?php endif; ?>
 
-					<div class="deal-share">
-						<?php echo do_shortcode( '[addtoany]' ); ?>
+									<?php while ( have_rows( 'image_slider' ) ) : the_row(); ?>
+										<div class="swiper-slide">
+											<div class="cover-image">
+												<?php echo wp_get_attachment_image( get_sub_field( 'image' ), 'deal' ); ?>
+											</div>
+										</div>
+									<?php endwhile; ?>
+							  </div>
+								<div class="swiper-pagination"></div>
+							</div>
+						<?php elseif ( has_post_thumbnail() ) : ?>
+							<div class="cover-image">
+								<?php the_post_thumbnail( 'deal' ); ?>
+							</div>
+						<?php else : ?>
+							<div class="cover-image">
+								<img src="<?php echo PJ_URI . 'assets/images/deal-placeholder.jpg'; ?>" alt="" />
+							</div>
+						<?php endif; ?>
 					</div>
-					<div class="deal-trustpilot">
-							<script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
-							<div class="trustpilot-widget" data-locale="en-GB" data-template-id="5406e65db0d04a09e042d5fc" data-businessunit-id="558011f70000ff0005803e84" data-style-height="28px" data-style-width="100%" data-theme="light"> <a href="https://uk.trustpilot.com/review/roundabouttravel.com.au" target="_blank" rel="noopener">Trustpilot</a> </div>
-					</div>
+				</div>
+			</section>
+		<?php endif; ?>
+
+		<section class="deal-trustpilot-ss">
+			<div class="container">
+				<div class="deal-trustpilot">
+					<script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
+					<div class="trustpilot-widget" data-locale="en-GB" data-template-id="5406e65db0d04a09e042d5fc" data-businessunit-id="558011f70000ff0005803e84" data-style-height="28px" data-style-width="100%" data-theme="light"> <a href="https://uk.trustpilot.com/review/roundabouttravel.com.au" target="_blank" rel="noopener">Trustpilot</a> </div>
 				</div>
 			</div>
 		</section>

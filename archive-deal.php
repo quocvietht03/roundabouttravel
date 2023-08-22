d27cdefced530f4f7cce154214f468cb440c37ca
quocvietht03 <quocvietht03@gmail.com>
2023-08-22 16:14:07 +0700

    create archive deal

--- archive-deal.php
+++ archive-deal.php
@@ -1,12 +1,9 @@
 <?php get_header(); ?>
 
-	<?php get_template_part( 'templates/hero', 'deal' ); ?>
-
 <style>
     .dp_notice_box{
         display: flex;
         background: #fff;
-        margin: 0 auto 70px auto;
         width: 100%;
         min-height: 70px;
     }
@@ -35,171 +32,132 @@
     }
 </style>
 
-	<div class="main">
+	<div class="deals-main">
+    <section class="deals-hero-ss">
+      <div class="container">
+				<div class="hero-content">
+          <h1 class="page-title">
+            <?php echo 'Airfare Deals'; ?>
+          </h1>
+          <?php echo do_shortcode('[aioseo_breadcrumbs]'); ?>
+        </div>
+      </div>
+		</section>
 
-		<div class="container">
+		<section class="deals-notice-ss">
+			<div class="container">
+				<div class="deals-notice-content">
+					<div class="dp_notice_box">
+							<div class="dp_icon">
+							</div>
+							<div class="dp_info">
+									<p>Round the world airfares are now on sale for departures until <?php echo date('F Y', mktime(0, 0, 0, date('m')+11, 1, date('Y'))); ?> with routes covering the most popular destinations in Europe and North America via major gateways in Asia.  </p>
+									<p>Book in advance to secure availability and preferred dates. View fares below and contact our airfare specialists for more information.  </p>
+							</div>
+					</div>
+				</div>
+			</div>
+		</section>
 
-			<div class="content-narrow">
-
-				<div id="main" role="main" class="clearfix">
-
-				    <div class="dp_notice_box">
-				        <div class="dp_icon">
-
-				        </div>
-				        <div class="dp_info">
-				            <p>Round the world airfares are now on sale for departures until <?php echo date('F Y', mktime(0, 0, 0, date('m')+11, 1, date('Y'))); ?> with routes covering the most popular destinations in Europe and North America via major gateways in Asia.  </p>
-										<p>Book in advance to secure availability and preferred dates. View fares below and contact our airfare specialists for more information.  </p>
-				        </div>
-				    </div>
-
-					<div class="search_deal">
-
+		<section class="deals-main-ss">
+			<div class="container">
+				<div class="deals-main-content">
+					<div class="deals-filter-form-wrap">
 						<h2>- Search Deals -</h2>
-
-						<form method="get" action="<?php bloginfo( 'url' ); ?>/deals/" class="clearfix clm4">
-
+						<form class="deals-filter-form" action="/deals/" method="get">
 							<input type="hidden" name="deal_search_submitted" value="1" />
-
 							<div class="box">
-
 								<label for="deal_search_class">Class Types</label>
-
 								<?php
-
-								wp_dropdown_categories( array(
-
-									'show_option_all' => '-- Any --',
-									'orderby'         => 'NAME',
-									'hide_empty'      => false,
-									'id'              => 'deal_search_class',
-									'name'            => 'deal_search_class',
-									'selected'        => $_GET['deal_search_class'],
-									'taxonomy'        => 'deal_class'
-
-								) );
-
+									wp_dropdown_categories( array(
+										'show_option_all' => '-- Any --',
+										'orderby'         => 'NAME',
+										'hide_empty'      => false,
+										'id'              => 'deal_search_class',
+										'name'            => 'deal_search_class',
+										'selected'        => $_GET['deal_search_class'],
+										'taxonomy'        => 'deal_class'
+									) );
 								?>
-
 							</div>
-
 							<div class="box">
-
 								<label for="deal_search_airline">Airline</label>
-
 								<?php
-
-								wp_dropdown_categories( array(
-
-									'show_option_all' => '-- Any --',
-									'orderby'         => 'NAME',
-									'hide_empty'      => false,
-									'id'              => 'deal_search_airline',
-									'name'            => 'deal_search_airline',
-									'selected'        => $_GET['deal_search_airline'],
-									'taxonomy'        => 'deal_airline'
-
-								) );
-
+									wp_dropdown_categories( array(
+										'show_option_all' => '-- Any --',
+										'orderby'         => 'NAME',
+										'hide_empty'      => false,
+										'id'              => 'deal_search_airline',
+										'name'            => 'deal_search_airline',
+										'selected'        => $_GET['deal_search_airline'],
+										'taxonomy'        => 'deal_airline'
+									) );
 								?>
-
 							</div>
-
 							<div class="box">
-
 								<label for="deal_search_sort">Sort by</label>
-
 								<select name="deal_search_sort" id="deal_search_sort">
 									<option value="price_asc" <?php selected( $_GET['deal_search_sort'], 'price_asc' ); ?>>Price Ascending</option>
 									<option value="price_desc" <?php selected( $_GET['deal_search_sort'], 'price_desc' ); ?>>Price Descending</option>
 									<option value="date_added" <?php selected( $_GET['deal_search_sort'], 'date_added' ); ?>>Date Added</option>
 								</select>
-
 							</div>
-
 							<div class="box">
-
 								<span class="btn"><input value="SEARCH" type="submit"></span>
-
 							</div>
-
 						</form>
-
 					</div>
 
-					<?php if ( have_posts() ) : ?>
+          <div class="deals-results">
+            <?php if ( have_posts() ) : ?>
+  						<div class="deal-items">
+  							<?php while ( have_posts() ) : the_post(); ?>
+  								<?php
+  								$price     = get_post_meta( $post->ID, 'deal_price', true );
+  								$price_tax = get_post_meta( $post->ID, 'deal_price_tax', true );
+  								?>
+  								<div class="deal-item">
+  									<div class="deal-thumbnail">
+  										<?php if ( has_post_thumbnail() ) : ?>
+  											<?php the_post_thumbnail( 'medium' ); ?>
+  										<?php endif; ?>
+  									</div>
 
-						<div class="deal_list">
+  									<div class="deal-content">
+  										<h2><?php the_title(); ?></h2>
+  										<?php the_excerpt(); ?>
+  										<div class="price"><span>From $<?php echo $price; ?></span> <?php if ( $price_tax == '1' ) : ?>inc taxes<?php endif; ?></div>
+  									</div>
 
-							<?php while ( have_posts() ) : the_post(); ?>
+  									<div class="deal-view-more">
+  										<a class="btn-view-more" href="<?php the_permalink(); ?>">View more</a>
+  									</div>
 
-								<?php
+  								</div>
+  							<?php endwhile; ?>
+  						</div>
 
-								$price     = get_post_meta( $post->ID, 'deal_price', true );
-								$price_tax = get_post_meta( $post->ID, 'deal_price_tax', true );
-
-								?>
-
-								<div class="deal clearfix">
-
-									<p class="img">
-
-										<?php if ( has_post_thumbnail() ) : ?>
-
-											<?php the_post_thumbnail( 'deal-thumb' ); ?>
-
-										<?php else : ?>
-
-											<img src="<?php echo get_template_directory_uri(); ?>/img/img_deal01.jpg" alt="" />
-
-										<?php endif; ?>
-
-									</p>
-
-									<div class="txt">
-
-										<h2><?php the_title(); ?></h2>
-
-										<?php the_excerpt(); ?>
-
-										<p class="price"><span>From $<?php echo $price; ?></span> <?php if ( $price_tax == '1' ) : ?>inc taxes<?php endif; ?></p>
-
-									</div>
-
-									<div class="viewmore">
-
-										<p class="btn"><a href="<?php the_permalink(); ?>">View more</a></p>
-
-									</div>
-
-								</div>
-
-							<?php endwhile; ?>
-
-						</div>
-
-						<?php //wp_pagenavi(); ?>
-
-					<?php else : ?>
-
-						<p>Your search returned no results.</p>
-
-					<?php endif; ?>
-
-					<br><br><br>
-
-					<!-- TrustBox widget - 0 -->
-					<div class="trustpilot-widget" data-locale="en-US" data-template-id="53aa8912dec7e10d38f59f36" data-businessunit-id="558011f70000ff0005803e84" data-style-height="140px" data-style-width="100%" data-theme="light" data-schema-type="Organization" data-stars="1,2,3,4,5">
-					  <a href="https://www.trustpilot.com/review/roundabouttravel.com.au" target="_blank">Trustpilot</a>
-					</div>
-					<!-- End TrustBox widget -->
-
-				</div><!--#main -->
-
+              <div class="pagination-nav">
+    						<?php echo paginate_links(); ?>
+              </div>
+  					<?php else : ?>
+  						<p>Your search returned no results.</p>
+  					<?php endif; ?>
+          </div>
+				</div>
 			</div>
+		</section>
 
-		</div>
-
+    <section class="deals-main-ss">
+			<div class="container">
+        <!-- TrustBox widget - 0 -->
+        <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
+    		<div class="trustpilot-widget" data-locale="en-US" data-template-id="53aa8912dec7e10d38f59f36" data-businessunit-id="558011f70000ff0005803e84" data-style-height="140px" data-style-width="100%" data-theme="light" data-schema-type="Organization" data-stars="1,2,3,4,5">
+    			<a href="https://www.trustpilot.com/review/roundabouttravel.com.au" target="_blank">Trustpilot</a>
+    		</div>
+    		<!-- End TrustBox widget -->
+      </div>
+    </section>
 	</div>
 
 <?php get_footer(); ?>

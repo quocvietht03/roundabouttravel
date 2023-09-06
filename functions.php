cb5e52e97a94e0806436cae89424350a0adb3afa
pidev077 <nguyenanhphi077@gmail.com>
2023-09-06 14:58:07 +0700

    sidebar news

--- functions.php
+++ functions.php
@@ -247,5 +247,16 @@ function be_sidebar_one_widgets_init() {
 			'after_title'   => '</h2>',
 		)
 	);
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar Archive', 'hello_elementor' ),
+			'id'            => 'sidebar-archive',
+			'description'   => esc_html__( 'Add widgets here to appear in your footer.', 'hello_elementor' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
 }
 add_action( 'widgets_init', 'be_sidebar_one_widgets_init' );

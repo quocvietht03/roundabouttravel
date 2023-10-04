ce02df3889bb2b43e04e20667a92ad41323507a1
Nguyễn Anh Phi <34386845+pidev077@users.noreply.github.com>
2023-10-04 20:38:25 +0700

    fix form
    fix form

--- package-main/assets/js/planmytrip-classic.js
+++ package-main/assets/js/planmytrip-classic.js
@@ -855,7 +855,7 @@ function loadStepScreen(stepScreenToLoad){
                 continentName = 'continentFourth';
             if(i == 4)
                 continentName = 'continentFifth';
-            $('#buildtripform').find('div.continent'+(i+1)+' > p.question').html('Where would you like to go in <h4>'+getContinentText(state[continentName][continentName])+' Cities</h4>, and how long would you like to stay?');
+            $('#buildtripform').find('div.continent'+(i+1)+' > p.question').html('Where would you like to go in <h3>'+getContinentText(state[continentName][continentName])+' Cities</h3>, and how long would you like to stay?');
             if(state[continentName][continentName] == 'asia'){
                 $('#buildtripform').find('div.continent'+(i+1)+' > section.asiatransit > input[type="checkbox"]').prop('id',$('#buildtripform').find('div.continent'+(i+1)+' > section.asiatransit > input[type="checkbox"]').prop('id')+(i+1)+'-1');
                 $('#buildtripform').find('div.continent'+(i+1)+' > section.asiatransit > label').prop('for',$('#buildtripform').find('div.continent'+(i+1)+' > section.asiatransit > label').prop('for')+(i+1)+'-1');

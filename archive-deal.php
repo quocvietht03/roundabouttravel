7b52be97cddf80b32b26192454ab3c9184bb4cb2
Nguyễn Anh Phi <34386845+pidev077@users.noreply.github.com>
2023-10-17 22:07:01 +0700

    Update archive-deal.php

--- archive-deal.php
+++ archive-deal.php
@@ -58,9 +58,9 @@
                 <?php if ( have_posts() ) : ?>
                 <div class="deal-items">
                     <?php while ( have_posts() ) : the_post(); ?>
-                    <a class="btn-more" href="<?php the_permalink(); ?>">
                     <div class="deal-item">
                         <div class="deal-thumbnail">
+                            <a class="btn-more" href="<?php the_permalink(); ?>">
                             <?php if ( has_post_thumbnail() ) : ?>
                             <?php the_post_thumbnail( 'medium_large' ); ?>
                             <?php endif; ?>
@@ -93,8 +93,9 @@
 								echo "</div>";
 							endif;
 							?>
+                            </a>
                         </div>
-
+                        <a class="btn-more" href="<?php the_permalink(); ?>">
                         <div class="deal-content">
                             <h2 class="deal-title"><?php the_title(); ?></h2>
                             <div class="deal-desc">
@@ -111,13 +112,12 @@
                         }
                       ?>
                         </div>
-
+                        </a>
                         <div class="deal-view-more">
-                            <p class="btn-view-more">View more</p>
+                            <a class="btn-view-more" href="<?php the_permalink(); ?>">View more</a>
                         </div>
                     
                     </div>
-                    </a>
                     <?php endwhile; ?>
                 </div>
 

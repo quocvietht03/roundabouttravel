7844550352343b796e53c8c5136dc6e6c157916b
pidev077 <nguyenanhphi077@gmail.com>
2023-11-02 11:12:38 +0700

    d

--- package-main/assets/js/main.js
+++ package-main/assets/js/main.js
@@ -43,6 +43,13 @@
     $(this).parent().append( "<div class='be-bg-blur'></div>" );
     $(this).parent().children(".be-bg-blur").attr('style', 'background-image: url("' + imageUrl +'")');
     $(this).parent().css({'display':'inline-block','position':'relative','overflow':'hidden','margin':'16px 0'});
-    $(this).parent().children(".be-bg-blur").css({'position':'absolute','background-size':'cover','filter':'blur(8px)','-webkit-filter':'blur(8px)','width':'100%','height':'calc(100% - 32px)','top':'16px','left':'0','z-index':'-1'});
-    });
+    $(this).parent().children(".be-bg-blur").css({'position':'absolute','background-size':'cover','filter':'blur(8px)','-webkit-filter':'blur(8px)','width':'100%','height':'100%','top':'0px','left':'0','z-index':'-1'});
+  });
+  $('.single .post-content-format figure img').each(function(){
+      var imageUrl = $(this).attr('src');
+      $(this).parent().append( "<div class='be-bg-blur'></div>" );
+      $(this).parent().children(".be-bg-blur").attr('style', 'background-image: url("' + imageUrl +'")');
+      $(this).parent().css({'display':'inline-block','position':'relative','overflow':'hidden','margin':'16px 0'});
+      $(this).parent().children(".be-bg-blur").css({'position':'absolute','background-size':'cover','filter':'blur(8px)','-webkit-filter':'blur(8px)','width':'100%','height':'100%','top':'0px','left':'0','z-index':'-1'});
+  });
 }) (jQuery);
